aefa8f9e18439fb251c874331cdece46882241d3
VishalH <vishal.harindrakumar@gmail.com>
2024-09-17 21:02:06 +0530

    package-lock changed

--- server/index.js
+++ server/index.js
@@ -1,21 +1,38 @@
-const {Server}=require("socket.io")
+const { Server } = require("socket.io");
 
 const io = new Server(8000, {
-    cors: true,
+  cors: true,
 });
 
-const userToSocketIdMap = new Map();
-const socketidToUserMap = new Map();
+const uidToSocketIdMap = new Map();
+const socketidToUidMap = new Map();
 
-io.on('connection',socket=>{
-    console.log(`Socket Connected ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { uid, room } = data;
+    uidToSocketIdMap.set(uid, socket.id);
+    socketidToUidMap.set(socket.id, uid);
+    io.to(room).emit("user:joined", { uid, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
 
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
 
-    socket.on("room:join",(data)=>{
-        const{username,room}=data;
-        
-        userToSocketIdMap.set(username,socket.id);
-        socketidToUserMap.set(socket.id,username);
-        io.to(socket.id).emit("room:join",data)
-    })
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
 });
